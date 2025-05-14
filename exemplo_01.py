bdc6b914fcd81d186321921db79d811e5b1429f4
Thales <contato.thales@gmail.com>
2025-05-13 21:12:52 -0300

    Adicionando o nosql para salvar as cotacoes

--- exemplo_01.py
+++ exemplo_01.py
@@ -1,7 +1,33 @@
 import requests
+from tinydb import TinyDB
+from datetime import datetime
+import time
 
-url = "https://api.coinbase.com/v2/prices/spot"
+def extrair():
+    url = "https://api.coinbase.com/v2/prices/spot"
+    response = requests.get(url)
+    return response.json()
 
-response = requests.get(url)
+def transformar(dados_json):
+    valor = dados_json['data']['amount']
+    criptomoeda = dados_json['data']['base']
+    moeda = dados_json['data']['currency']
+    dados_tratados = {
+        "valor": valor,
+        "criptomoeda": criptomoeda,
+        "moeda": moeda,
+        "timestamp": datetime.now().isoformat()
+                }
+    return dados_tratados
 
-print(response.json())
+def load(dados_tratados):
+    db = TinyDB('db.json')
+    db.insert(dados_tratados)
+    print("Dados salvos com sucesso!")
+
+if __name__ == "__main__":
+    while True:
+        dados_json = extrair()
+        dados_tratados = transformar(dados_json)
+        load(dados_tratados)
+        time.sleep(5)
